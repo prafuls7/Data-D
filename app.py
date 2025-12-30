52eb72f9ed509513852557de1c9f0138b7841457
Praful Shukla <86353711+prafuls7@users.noreply.github.com>
2025-12-30 15:06:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import random
 import re
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
+from typing import Dict  # Add this import
 
 warnings.filterwarnings('ignore')
 
